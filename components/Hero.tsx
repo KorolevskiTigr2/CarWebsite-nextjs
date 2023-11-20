e64c0d27cc610259860552cc660db94d15f1fb27
Alexander Medvedev <mega.medved87@gmail.com>
2023-11-20 14:06:07 -0600

    Create CustomButton.tsx and types folder

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,37 @@
+"use client";
+
 import Image from 'next/image';
+import CustomButton from "./CustomButton"
 
 const Hero = () => {
+    const handleScroll = () => {
+        console.log("I pushed the button")
+    }
+
+
+
   return (
-    <div>Hero</div>
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                Scooby-Doo is coming!
+            </h1>
+
+            <p className='hero__subtitle'>
+              Scooby-Dooby-Doo, Where Are You?
+                We got some work to do now.
+                 Scooby-Dooby-Doo, Where Are You?
+                  We need some help from you now.
+            </p>
+
+            <CustomButton 
+                title="Explore Scooby Doo"
+                containerStyles="bg-primary-blue
+                text-white rounded-full mt-10"
+                handleClick={handleScroll}
+            />
+        </div>
+    </div>
   )
 }
 
